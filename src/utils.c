21c6338f9b87ec5ffd2de76efcf60e69f278f1d7
Setoa <diqzk528@naver.com>
2017-10-03 08:04:27 -0700

    function1 clear

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,23 @@
 #include "utils.h"
-
+#include <string.h>
+#include <stdlib.h>
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+	char** arr=(char**)malloc(sizeof(char*));
+	int count=0;
+	char* token;
+	char* cmd=(char*)malloc(sizeof(char));
+	strcpy(cmd,command);
+	token=strtok(cmd," ");
+	while(token!=NULL)
+	{
+		arr[count]=(char*)malloc(sizeof(char));
+		strcpy(arr[count],token);
+		token=strtok(NULL," ");
+		count++;
+	}
+	*argc=count;
+	*argv=arr;
 }
