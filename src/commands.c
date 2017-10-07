ce7ba6b78bf6a930e5b6f5761c15596ef23079ea
Setoa <diqzk528@naver.com>
2017-10-07 05:47:33 -0700

    All Clear!

--- src/commands.c
+++ src/commands.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
-
+#define BUFSIZE 128
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+  if(chdir(argv[1])) return -1;
   // TODO: Fill it!
   return 0;
 }
@@ -13,7 +14,9 @@ int do_cd(int argc, char** argv) {
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
+  char curdir_buf[BUFSIZE];
+  getcwd(curdir_buf,BUFSIZE);
+  printf("%s\n",curdir_buf);
   // TODO: Fill it!
 
   return 0;
@@ -21,10 +24,12 @@ int do_pwd(int argc, char** argv) {
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
+  if(strcmp(argv[0],"cd")||argc!=2) return 0;
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
+  if(argc!=1||strcmp(argv[0],"pwd")) return 0;
   return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,23 +1,30 @@
 #include "utils.h"
 #include <string.h>
 #include <stdlib.h>
+#define MAXSIZE 100
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
-	char** arr=(char**)malloc(sizeof(char*));
+	*argv=(char**)malloc(sizeof(char*)*MAXSIZE);
 	int count=0;
 	char* token;
-	char* cmd=(char*)malloc(sizeof(char));
+	char* cmd=(char*)malloc(sizeof(char)*MAXSIZE);
 	strcpy(cmd,command);
-	token=strtok(cmd," ");
+	token=strtok(cmd," \n\t");
+	if(token==NULL)
+	{
+		*argc=1;
+		**argv=(char*)malloc(sizeof(char)*MAXSIZE);
+		strcpy(**argv,"");
+		return;
+	}
 	while(token!=NULL)
 	{
-		arr[count]=(char*)malloc(sizeof(char));
-		strcpy(arr[count],token);
-		token=strtok(NULL," ");
+		(*argv)[count]=(char*)malloc(sizeof(char)*100);
+		strcpy((*argv)[count],token);
+		token=strtok(NULL, " \n\t");
 		count++;
 	}
 	*argc=count;
-	*argv=arr;
 }
